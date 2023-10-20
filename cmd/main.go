64317965df61048229b1b48ee4a61c383e7fe126
FosenTS <juster050@yandex.com>
2023-10-21 02:40:27 +0300

    create application logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"bering-travel-api/internal/application"
+	"bering-travel-api/internal/application/config"
+	"context"
+	"flag"
+	"fmt"
+	mylogging "github.com/FosenTS/pkg/mylogger"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/lann/builder"
+	"log"
+	"time"
+)
+
+var modeFlag *string
+
+func main() {
+	time.Local = time.UTC
+	modeFlag = flag.String("mode", config.Mode.Local(), "specifies whether program is for dev or prod or local usage, default is local")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer func() {
+		if cancel != nil {
+			cancel()
+		}
+	}()
+	flag.Parse()
+	mode := *modeFlag
+	switch mode {
+	case config.Mode.Local():
+		log.Println("Local mode!")
+	case config.Mode.Dev():
+		log.Println("Dev mode!")
+	case config.Mode.Prod():
+		log.Println("Prod mode!")
+	default:
+		log.Fatalln("invalid mode")
+	}
+	config.LoadEnv(mode)
+
+	mylogging.ConfigureLogrus()
+	rootLogger := mylogging.GetLogger()
+
+	rootLogger.Infoln("Let's go!")
+	rootLogger.Infoln("Creating app...")
+
+	sq.StatementBuilder = sq.StatementBuilderType(builder.EmptyBuilder).PlaceholderFormat(sq.Dollar)
+
+	app, err := application.NewApp(ctx, rootLogger.WithContext(ctx), nil)
+	if err != nil {
+		cancel()
+		time.Sleep(time.Microsecond * 300)
+		rootLogger.Fatalf(fmt.Sprintf("fatal creating app: %s\n", err))
+	}
+	rootLogger.Infoln("Starting app...")
+	err = app.Run(ctx)
+	if err != nil {
+		cancel()
+		time.Sleep(time.Microsecond * 300)
+		log.Fatalf("fatal starting or while running app: %s\n", err)
+	}
+}
---
+++ internal/application/application.go
@@ -0,0 +1,136 @@
+package application
+
+import (
+	"bering-travel-api/internal/application/config"
+	"bering-travel-api/internal/application/product"
+	"context"
+	"fmt"
+	"github.com/FosenTS/pkg/db/postgresql"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"time"
+)
+
+type App interface {
+	Run(ctx context.Context) error
+	runHTTP() error
+}
+
+type app struct {
+	cfg       config.AppConfig
+	pgPool    postgresql.PGXPool
+	endpoint  *product.Endpoint
+	logEntry  *log.Entry
+	ginLogger gin.HandlerFunc
+}
+
+func NewApp(ctx context.Context, logEntry *log.Entry, ginLogger gin.HandlerFunc) (App, error) {
+
+	// Build postgres pool
+	log.Infoln("Creating pgxpool")
+	postgresConfig := config.Postgres()
+	pgPool, err := postgresql.NewPGXPool(
+		ctx,
+		&postgresql.ClientConfig{
+			MaxConnections:        postgresConfig.MaxConnections,
+			MaxConnectionAttempts: postgresConfig.MaxConnectionAttempts,
+			WaitingDuration:       postgresConfig.WaitTimeout,
+			Username:              postgresConfig.Username,
+			Password:              postgresConfig.Password,
+			Host:                  postgresConfig.Host,
+			Port:                  postgresConfig.Port,
+			DatabaseName:          postgresConfig.DatabaseName,
+			UseCA:                 postgresConfig.UseCA,
+			CaAbsPath:             postgresConfig.CAAbsPath,
+			SimpleQueryMode:       postgresConfig.SimpleQueryMode,
+		},
+		logEntry.WithField("location", "pgxpool"),
+	)
+	if err != nil {
+		log.Errorf("error creating pgxpool: %s\n", err)
+		return nil, err
+	}
+	log.Warnf("This is pgxpool: %v\n", pgPool)
+
+	// Create repositories
+	log.Infoln("Creating repository...")
+	repos := product.NewStorages(pgPool)
+
+	// Create services
+	log.Infoln("Creating services...")
+	services, err := product.NewServices(repos)
+	if err != nil {
+		return nil, fmt.Errorf("fatal couldn't create services: %w", err)
+	}
+
+	// Create gateways
+	log.Infoln("Gateways layer")
+	gateways, err := product.NewGateways(
+		services,
+	)
+
+	// Controllers
+	log.Infoln("Creating controllers")
+	controllers, err := product.NewControllers(
+		gateways,
+		config.Handler(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("fatal couldn't create controllers: %s", err)
+	}
+
+	// Endpoint artificial-layer
+	log.Infoln("Creating artificial endpoint layer")
+	endpoint := product.NewEndpoint(controllers)
+
+	return &app{
+		cfg:       config.App(),
+		endpoint:  endpoint,
+		pgPool:    pgPool,
+		logEntry:  logEntry,
+		ginLogger: ginLogger,
+	}, nil
+}
+
+func (app *app) Run(ctx context.Context) error {
+
+	grp, ctx := errgroup.WithContext(ctx)
+
+	grp.Go(func() error {
+		return app.runHTTP()
+	})
+
+	return nil
+}
+
+func (app *app) runHTTP() error {
+	r := gin.New()
+	r.Use(gin.LoggerWithConfig(gin.LoggerConfig{Output: os.Stdout}), gin.Recovery())
+
+	err := app.endpoint.RegisterHandlersToGroup(r)
+	if err != nil {
+		return err
+	}
+
+	addr := app.cfg.HTTP.IP
+	if len(app.cfg.HTTP.Port) != 0 {
+		addr += ":" + app.cfg.HTTP.Port
+	}
+
+	for {
+		err = r.Run(addr)
+		if err != nil {
+			log.Errorf("error running http server: %s", err)
+		} else {
+			log.Warnln("warn running http server: it stopped without error")
+		}
+		if app.cfg.IsDev {
+			log.Errorf("error running HTTP controller")
+			return err
+		} else {
+			time.Sleep(app.cfg.RevokeStunDuration)
+		}
+	}
+}
